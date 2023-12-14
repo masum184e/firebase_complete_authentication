bf126df68b20d6967070d4cb388a4cef0c20d460
masum184e <masum184e@gmail.com>
2023-12-14 10:44:53 +0600

    error element added

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,16 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Registration from "../pages/Registration";
+import NotFound from "../pages/NotFound";
 
 const router = createBrowserRouter([
     {
         path: "/",
+        errorElement: <NotFound />
     }, {
         path: "/login",
-        element: <Login />
+        element: <Login />,
+        errorElement: <NotFound />
     }, {
         path: "/registration",
-        element: <Registration />
+        element: <Registration />,
+        errorElement: <NotFound />
     }
 ])
 
