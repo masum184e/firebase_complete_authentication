4685550103045fe9e8115939a5d33368011e5999
masum184e <masum184e@gmail.com>
2024-05-31 23:36:53 +0600

    google added

--- src/components/SocialAuthentication.jsx
+++ src/components/SocialAuthentication.jsx
@@ -1,27 +1,30 @@
-import { FcGoogle } from 'react-icons/fc'
+import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
+import useAuth from "../hooks/useAuth";
 
 const SocialAuthentication = () => {
-    const loginWithGoogle = async () => {
+  const { googleLogin } = useAuth();
+  const loginWIthFacebook = async () => {};
+  return (
+    <>
+      <div className="space-y-2">
+        <button
+          onClick={googleLogin}
+          className="btn btn-outline border-warning btn-block hover:bg-white hover:text-black"
+        >
+          <FcGoogle className="text-xl" />
+          <span>Continue With Google</span>
+        </button>
+        <button
+          onClick={loginWIthFacebook}
+          className="btn btn-outline border-warning btn-block hover:bg-white hover:text-black"
+        >
+          <FaFacebook className="text-xl text-[#0866FF]" />
+          <span>Continue With Facebook</span>
+        </button>
+      </div>
+    </>
+  );
+};
 
-    }
-    const loginWIthFacebook = async () => {
-
-    }
-    return (
-        <>
-            <div className="space-y-2">
-                <button onClick={loginWithGoogle} className="btn btn-outline border-warning btn-block hover:bg-white hover:text-black" >
-                    <FcGoogle className="text-xl" />
-                    <span>Continue With Google</span>
-                </button>
-                <button onClick={loginWIthFacebook} className="btn btn-outline border-warning btn-block hover:bg-white hover:text-black" >
-                    <FaFacebook className="text-xl text-[#0866FF]" />
-                    <span>Continue With Facebook</span>
-                </button>
-            </div>
-        </>
-    )
-}
-
-export default SocialAuthentication
+export default SocialAuthentication;
