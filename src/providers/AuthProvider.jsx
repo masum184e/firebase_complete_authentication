9b18b456b70294f7be17e56619996e46b947cb4d
masum184e <masum184e@gmail.com>
2024-05-31 23:36:21 +0600

    firebase method added

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,14 @@
 import { createContext, useEffect, useState } from "react";
-import PropTypes from 'prop-types'
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import PropTypes from "prop-types";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 import firebaseApp from "../firebase/firebase.config";
 
@@ -8,31 +16,55 @@ export const AuthContext = createContext(null);
 const auth = getAuth(firebaseApp);
 
 const AuthProvider = ({ children }) => {
-    const [loggedInUserData, setLoggedInUserData] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [loggedInUserData, setLoggedInUserData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setLoggedInUserData(currentUser);
-            setLoading(false);
-        })
-        return () => {
-            unsubscribe();
-        }
-    }, [])
+  const registerUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const googleLogin = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-    const authInfo = {
-        loggedInUserData,
-        setLoggedInUserData,
-        loading,
-        setLoading
-    }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setLoggedInUserData(currentUser);
+      setLoading(false);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
-    return <AuthContext.Provider value={authInfo}> {children} </AuthContext.Provider>
-}
+  const authInfo = {
+    loggedInUserData,
+    setLoggedInUserData,
+    loading,
+    setLoading,
+    registerUser,
+    loginUser,
+    googleLogin,
+    logOut,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}> {children} </AuthContext.Provider>
+  );
+};
 
 AuthProvider.propTypes = {
-    children: PropTypes.node.isRequired
-}
+  children: PropTypes.node.isRequired,
+};
 
 export default AuthProvider;
