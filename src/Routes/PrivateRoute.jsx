48f824e46ad1e99b9462ac30408d8134ebfa8cc1
masum184e <masum184e@gmail.com>
2024-06-13 17:54:56 +0600

    loader added

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,13 +1,14 @@
 import PropTypes from "prop-types";
 import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
+import Loader from "../components/Loader";
 
 const PrivateRoute = ({ children }) => {
   const { loading, loggedInUserData } = useAuth();
   const location = useLocation();
 
   if(loading){
-    return "Loading"
+    return <Loader />
   }
 
   if (loggedInUserData) {
---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="flex justify-center items-center mt-60">
+        <img src="/images/loader.gif" alt="LOADER" />
+    </div>
+  )
+}
+
+export default Loader
